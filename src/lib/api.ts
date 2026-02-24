5d232b742b0ba2fccacf1c624382840245271bfd
N-Flat <german300700gassy@gmail.com>
2026-02-24 22:05:12 +0900

    Enable withCredentials for Axios instance

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -7,6 +7,7 @@ const API_BASE_URL =
 // Axiosインスタンス作成
 const apiClient = axios.create({
   baseURL: API_BASE_URL,
+  withCredentials: true,
   headers: {
     'Content-Type': 'application/json',
     'Accept': 'application/json',
